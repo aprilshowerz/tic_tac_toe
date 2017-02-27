00fb7e85acecd1f5005f292358b313fa94badffe
April Blair <a.m.blair@eagle.clarion.edu>
2017-02-27 16:51:34 -0500

    added tests and functions

--- board.rb
+++ board.rb
@@ -10,13 +10,43 @@ class Board
 	end
 
 	def open_spot?(available)
-		if ttt_board[position] == ''
+		if ttt_board[available] == '' 
 			true
 		else
 			false
 		end
 	end
 
+	def full_board?
+		if ttt_board.include('')
+			false
+		else
+			true
+		end
+	end
 
+	def valid_input?(input)
+		if input.match(/[XxOo]/)
+			true
+		else
+			false
+		end
+	end
+
+	def game_tie?
+		if ttt_board.include?('')
+			false
+		else
+			true
+		end
+	end
+
+	def winner?
+		if ttt_board == ''
+
+		else
+
+		end
+	end
 end
 
--- test_board.rb
+++ test_board.rb
@@ -27,4 +27,43 @@ class TestTicTacToe < Minitest::Test
 		board.update_position(8, 'X')
 		assert_equal(['X', 'O', '', 'O', '', '', '', '', 'X'], board.ttt_board)
 	end
+
+	def test_open_spot
+		board = Board.new
+		board.ttt_board = (['X', 'O', '', 'O', '', '', '', '', 'X'])
+		assert_equal(true, board.open_spot?(6))
+		assert_equal(false, board.open_spot?(0))
+		assert_equal(false, board.open_spot?(15))
+		assert_equal(true, board.open_spot?(8))
+	end
+
+	def test_for_full_board_false
+		board = Board.new
+		board.ttt_board = (['X', 'X', 'O', 'O', 'O', 'X', '', 'O', 'X'])
+		assert_equal(false, board.full_board?)
+	end
+
+	def test_for_full_board_true
+		board = Board.new
+		board.ttt_board = (['X', 'X', 'O', 'O', 'O', 'X', 'X', 'O', 'X'])
+		assert_equal(true, board.full_board?)
+	end
+	def test_valid_input
+		board = Board.new
+		assert_equal(false, board.valid_input?('m'))
+		assert_equal(true, board.valid_input?('x'))
+		assert_equal(true, board.valid_input?('X'))
+		assert_equal(true, board.valid_input?('o'))
+		assert_equal(true, board.valid_input?('O'))
+		assert_equal(false, board.valid_input?('4'))
+		assert_equal(false, board.valid_input?(' '))
+	end
+
+	def test_winner_x_at_position_0_1_2_with_x
+		board = Board.new
+		marker = 'X'
+		board.ttt_board = ['X', 'X', 'X', 'O', '', 'O', '', 'O', '']
+		assert_equal(true, board.winner?(marker))
+
+	end
 end

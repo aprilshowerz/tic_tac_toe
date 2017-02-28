05870159628ac46e1ec241da206d837a124a0cad
April Blair <a.m.blair@eagle.clarion.edu>
2017-02-28 12:53:24 -0500

    updated tests and functions

--- board.rb
+++ board.rb
@@ -1,31 +1,28 @@
 class Board
 
 	attr_accessor :ttt_board
-	def initialize
+
+	def initialize 
 		@ttt_board = Array.new(9, '')
 	end
+
 
 	def update_position(position, marker)
 		ttt_board[position] = marker
 	end
 
-	def open_spot?(available)
-		if ttt_board[available] == '' 
+
+	def open_position?(position)
+		if ttt_board[position] == ''
 			true
 		else
-			false
+			false 
 		end
 	end
 
-	def full_board?
-		if ttt_board.include('')
-			false
-		else
-			true
-		end
-	end
 
 	def valid_input?(input)
+
 		if input.match(/[XxOo]/)
 			true
 		else
@@ -33,20 +30,47 @@ class Board
 		end
 	end
 
-	def game_tie?
+
+	def full_board?()
+
 		if ttt_board.include?('')
 			false
 		else
 			true
 		end
+
 	end
 
-	def winner?
-		if ttt_board == ''
 
-		else
+	def winner?(marker)
 
+	win_array = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]] 
+
+	results = false
+
+		win_array.each do |win|
+			row = 0
+			
+			win.each do |pos|
+
+
+				if ttt_board[pos] == marker
+					row += 1
+
+					if row == 3
+						results = true
+					end
+				end
+			end
 		end
+		results
 	end
-end
 
+
+
+
+
+
+
+
+end

218c0ea159fd99a95107e9c92d0a45a538b3f974
April Blair <a.m.blair@eagle.clarion.edu>
2017-03-16 12:32:12 -0400

    started app

--- class version/app.rb
+++ class version/app.rb
@@ -1,16 +1,26 @@
 require 'sinatra'
-# require_relative 'TODO -CHANGE FROM CONSOLE GAME'
+require 'rubygems'
 require_relative 'board.rb'
+require_relative 'random.rb'
+require_relative 'sequel.rb'
 require_relative 'unbeatable.rb'
-require_relative 'player_classes.rb'
+require_relative 'human.rb'
 
-class TicTacToe < Sinatra::Base
+get '/' do
 
-	get '/' do
+erb :player_input
 
-		erb :welcome
+end
 
-	end
+post '/player' do 
+	p1_name = params[:player_1]
+	p2_name = params[:player_2]
 
+	redirect '/board?p1_name=' + p1_name + '&p2_name=' + p2_name
+end
 
+get '/board' do
+	p1_name = params[:p1_name]
+	p2_name = params[:p2_name]
+	erb  :board, :locals => {p1_name: p1_name, p2_name: p2_name}
 end

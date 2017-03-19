a5a8935127325aca9204cf619036715c57d3c104
April Blair <a.m.blair@eagle.clarion.edu>
2017-03-19 17:38:01 -0400

    test that marker changes to x or o.

--- tictactoe_ruby/test_board_logic.rb
+++ tictactoe_ruby/test_board_logic.rb
@@ -7,5 +7,10 @@ class TicTacToe < Minitest::Test
 		assert_equal([1, 2, 3, 4, 5, 6, 7, 8, 9], create_new_board)
 	end
 
+	def test_that_marker_changes_to_X_or_O
+		assert_equal([1, 2, 3, "X", 5, 6, 7, 8, 9], board_update([1, 2, 3, 4, 5, 6, 7, 8, 9], 3, "X"))
+		assert_equal([1, "O", 3, 4, 5, 6, 7, 8, 9], board_update([1, 2, 3, 4, 5, 6, 7, 8, 9], 1, "O"))
+	end
+
 	
 end

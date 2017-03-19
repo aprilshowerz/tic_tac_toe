faa74d36855532a674ae2e62bf3f7d55f8937fe7
April Blair <a.m.blair@eagle.clarion.edu>
2017-03-19 16:04:53 -0400

    cleaned up folder deleted, renamed. trying sessions. restarted app.rb

--- app.rb
+++ app.rb
@@ -1,26 +1,15 @@
 require 'sinatra'
-require 'rubygems'
-# require_relative 'board.rb'
-# require_relative 'random.rb'
-# require_relative 'sequel.rb'
-# require_relative 'unbeatable.rb'
-# require_relative 'human.rb'
+require_relative 'tictactoe_ruby/board_logic.rb'
+require_relative 'tictactoe_ruby/ttt_ai_game.rb'
+require_relative 'tictactoe_ruby/tictactoe_sai.rb'
+require_relative 'tictactoe_ruby/tictactoe_seq_sai.rb'
+require_relative 'tictactoe_ruby/human.rb'
 
-get '/' do
 
-#erb :player_input
-erb :get_name
+enable :sessions
+
+get '/' do 
+	erb :choose_player
+	
 end
 
-# post '/player' do 
-# 	p1_name = params[:player_1]
-# 	p2_name = params[:player_2]
-
-# 	redirect '/board?p1_name=' + p1_name + '&p2_name=' + p2_name
-# end
-
-# get '/board' do
-# 	p1_name = params[:p1_name]
-# 	p2_name = params[:p2_name]
-# 	erb  :board, :locals => {p1_name: p1_name, p2_name: p2_name}
-# end

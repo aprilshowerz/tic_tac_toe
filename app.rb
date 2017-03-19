6fa54894d1a3b002cd1d43c34036f313cf767a52
April Blair <a.m.blair@eagle.clarion.edu>
2017-03-19 16:07:22 -0400

    post to playermarker

--- app.rb
+++ app.rb
@@ -18,3 +18,15 @@ post '/input' do
 	session[:player_two] = params[:player_two] 
  	erb :player_one_marker,:locals => {:player_one => session[:player_one], :player_two => session[:player_two]} 
 end 
+
+post '/playermarker' do
+	player_one_marker = params[:player_one_marker]
+	player_two_marker = params[:player_two_marker]
+
+	if player_one_marker == "yes"
+		player_one_marker = "X"
+		player_two_marker = "O"
+	elsif player_one_marker == "no"
+		player_two_marker = "X"
+		player_one_marker = "O"
+	end

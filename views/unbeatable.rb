936ffdb5a136c3d1d1bb7642aadb331bdf4ac131
April Blair <a.m.blair@eagle.clarion.edu>
2017-03-19 16:58:16 -0400

    added center function

--- views/unbeatable.rb
+++ views/unbeatable.rb
@@ -6,6 +6,15 @@ class Perfect
 	 board_array = %w[1 2 3 4 5 6 7 8 9]
 	end
 
-	
+	#center position move
+	def center(board, index)
+		if board[4] = index + 1
+			true
+		else 
+			false
+		end
+	end
+
+
 
 end

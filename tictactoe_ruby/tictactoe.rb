3f40d7d48fe33c575357474b03239ea035fd1310
April Blair <a.m.blair@eagle.clarion.edu>
2017-03-19 17:52:57 -0400

    printgameboard method

---
+++ tictactoe_ruby/tictactoe.rb
@@ -0,0 +1,11 @@
+def print_game_board(array)
+    array
+    puts """
+      #{array[0]} | #{array[1]} | #{array[2]}  
+     -----------
+      #{array[3]} | #{array[4]} | #{array[5]} 
+     -----------
+      #{array[6]} | #{array[7]} | #{array[8]} """
+end
+
+

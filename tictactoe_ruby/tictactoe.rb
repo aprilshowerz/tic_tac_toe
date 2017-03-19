ee62d98c5aa13fdfd1c881608f2bd79e449af081
April Blair <a.m.blair@eagle.clarion.edu>
2017-03-19 17:53:35 -0400

    adk to play game method

--- tictactoe_ruby/tictactoe.rb
+++ tictactoe_ruby/tictactoe.rb
@@ -8,4 +8,14 @@ def print_game_board(array)
       #{array[6]} | #{array[7]} | #{array[8]} """
 end
 
+def ask_to_play_game
+    puts "Would you like to play a game of tic-tac-toe?"
+    answer_to_play_game = gets.chomp.upcase
+    if answer_to_play_game == "YES"
+        
+    else
+        puts "The game has been ended."
+        exit
+    end
+end
 
